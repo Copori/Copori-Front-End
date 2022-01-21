4b608f5da13cffa8ec578e17496258f3d570a25d
gyus13 <65529348+gyus13@users.noreply.github.com>
2022-01-21 10:49:05 +0900

    fitst

---
+++ src/Routes/main/Main.js
@@ -0,0 +1,15 @@
+import "../style/style.css";
+import Title from "./Title/Title";
+import GoodBooks from "./GoodBooks/GoodBooks";
+
+function Main() {
+  //View
+  return (
+    <div className="main">
+      <Title />
+      <GoodBooks />
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/Routes/main/Title/Title.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+import {Link} from "react-router-dom";
+import { AiOutlineRead, AiOutlineKey } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs";
+
+function Title() {
+  const [toggle, setToggle] = useState(true);
+  let isAuthorized = localStorage.getItem("JWT");
+
+  const onClickLogout = () => {
+    localStorage.removeItem('JWT')
+    window.location.replace("/")
+  };
+
+
+  return (
+    <div className="main__title">
+      <div className="main__title__box-text">
+        <span>Read&Review</span>
+      </div>
+      <div className="main__title__box-icon">
+        <div className="box-icon__contents">
+          <Link to="/Detail">
+          <AiOutlineRead />
+          <span>서재</span>
+          </Link>
+        </div>
+        <div>
+          {toggle ? (
+            <div className="box-icon__contents">
+              {!isAuthorized 
+              ? 
+              <Link to="/login">
+                <AiOutlineKey />
+                <span>로그인</span>
+                </Link>
+              : 
+                <span onClick={onClickLogout}>
+                <AiOutlineKey />
+                로그아웃
+                </span>
+                
+              }
+            </div>
+          ) : (
+            <div className="box-icon__contents">
+              <BsFillPersonFill />
+              <span>내 정보</span>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
