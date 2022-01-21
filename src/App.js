0a7b2bf8820de7c201f007bf677ebcff16491226
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-01-21 20:29:12 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./Routes/main/Main";
-import Login from "./Routes/login/Login";
-import BookDetail from "./Routes/bookDetail/BookDetail";
-import Signup from "./Routes/signUp/Signup";
-import Profile from "./Routes/profile/Profile";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './Routes/main/Main';
+import Login from './Routes/login/Login';
+import BookDetail from './Routes/bookDetail/BookDetail';
+import Signup from './Routes/signUp/Signup';
+import Profile from './Routes/profile/Profile';
 
 function App() {
   return (
@@ -11,15 +11,9 @@ function App() {
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Main />} />
         <Route path={`${process.env.PUBLIC_URL}/login`} element={<Login />} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/books`}
-          element={<BookDetail />}
-        />
+        <Route path={`${process.env.PUBLIC_URL}/books`} element={<BookDetail />} />
         <Route path={`${process.env.PUBLIC_URL}/signUp`} element={<Signup />} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/profile`}
-          element={<Profile />}
-        />
+        <Route path={`${process.env.PUBLIC_URL}/profile`} element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
